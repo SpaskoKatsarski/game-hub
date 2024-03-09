daa3d7bb2d291c8a5c46dd68b47690b88dfa7920
Spasko <patsokatsarski1@gmail.com>
2024-03-09 12:09:57 +0200

    Implementing theme change

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,16 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image, Text, useColorMode, Button } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 
 const NavBar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <HStack>
       <Image src={logo} boxSize="60px" />
       <Text>Text</Text>
+      <Button onClick={toggleColorMode}>
+        Toggle {colorMode === "light" ? "Dark" : "Light"}
+      </Button>
     </HStack>
   );
 };
